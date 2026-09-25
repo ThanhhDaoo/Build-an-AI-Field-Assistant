c9411c5af13a6d377ac2ceebb51a57fa21b65840
ThanhhDaoo <tranthanhdao132005@gmail.com>
2026-09-25 17:34:32 +0700

    deploy: cap nhat web build va deploy thanh cong len Vercel

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3984529679" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "795537498" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
